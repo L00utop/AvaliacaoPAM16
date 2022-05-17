b2f4b9f39cb516f6658f80218456b7a570573dca
Gabriel Felipe Kalaqui Rodrigues <L00ut2312@gmail.com>
2022-05-16 22:29:04 -0300

    primeira commit

---
+++ App.js
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Image , View } from 'react-native';
+
+import Img from './assets/Lua.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.img} source={Img}/>
+
+    
+    <View style={styles.boxtexto} >
+    <Text style={styles.titulo}>Lua</Text>
+    <Text style={styles.texto}>A lua é o satélite natural do <Text style={styles.especial}>planeta Terra</Text>, distanciados por aproximadamente 384.405km.</Text>
+    </View>
+    <Text style={styles.assinatura}>Feito por: Gabriel Felipe, Miguel Pedro</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30
+  },
+
+  img: {
+    width: 100,
+    height: 100,
+    marginBottom: 50
+  },
+
+  boxtexto: {
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 20,
+    borderStyle: 'dotted',
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+
+  titulo: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+
+  texto: {
+    fontSize: 20,
+    color: '#fff',
+    textAlign: 'center'
+  },
+
+  especial: {
+    fontStyle: 'italic',
+    fontWeight: 'bold'
+  },
+  
+  assinatura: {
+    color: '#fff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    padding: 30
+  }
+});
